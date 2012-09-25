2cd52b42fa86afed404a6230da2dd4827418e5d3
Adam Haley <adamhaley@Adams-MacBook-Pro.local>
2012-09-24 22:24:07 -0700

    Added Newest article to press page.

--- content/press.php
+++ content/press.php
@@ -1,4 +1,19 @@
-<div><a href="http://lahotmagazine.com/general-la-hot-magazine-news/322" target="_new"><img src="images/RachelBowman-Aerial-Reach-IMG_4836.jpg" style="float:left;margin:10px;" border="0px"/></a>
+<div class="press"><a href="http://www.examiner.com/article/objectdefy-founder-rachel-bowman-music-movement-and-the-art-of-aerial-dance" target="_new"><img src="images/thing.jpeg" style="float:left;margin:10px;width:100px;" border="0px"/></a>
+
+<div>
+<br />
+<a href="http://www.examiner.com/article/objectdefy-founder-rachel-bowman-music-movement-and-the-art-of-aerial-dance" target="_new">Interview in Examiner Magazine - 9 - 20 -2012</a><br /><br />
+
+Rachel Bowman, creative director of ObjectDefy Aerial Entertainment, discovered music at a very young age. But it was more than the songs and lyrics that touched her - it was movement and dance as a means of communicating emotions. Growing up in Australia, she studied ballet and found the path to self-expression and a career...
+[<a href="http://www.examiner.com/article/objectdefy-founder-rachel-bowman-music-movement-and-the-art-of-aerial-dance" target="_new">Read More</a>]
+</div>
+
+</div>
+
+<br /><br />
+
+
+<div class="press"><a href="http://lahotmagazine.com/general-la-hot-magazine-news/322" target="_new"><img src="images/RachelBowman-Aerial-Reach-IMG_4836.jpg" style="float:left;margin:10px;" border="0px"/></a>
 
 <div>
 <br />
